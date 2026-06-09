a6cfef393152905703e4de4253454b8e121dc8a6
punnittt <91917716+punnittt@users.noreply.github.com>
2026-06-09 08:28:28 +0000

    Variables and datatypes

---
+++ 01_Basics/01_Datatypes.js
@@ -0,0 +1,5 @@
+let name = "Punit"
+let number = 23
+let empty = null
+
+console.log(typeof empty)
---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = 121212
+let accountEmail = "punit@google.com"
+var accountPassword = "20528"
+let accountNo = " ";
+
+console.table([accountId, accountEmail, accountPassword, accountNo])
